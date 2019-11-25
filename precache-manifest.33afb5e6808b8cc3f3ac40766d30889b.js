bd9a8684fb1beca4fbc1821a01e896cf068983f0
shaz13 <shahebaz13@gmail.com>
2019-11-25 07:47:14 +0530

    Updates

---
+++ precache-manifest.33afb5e6808b8cc3f3ac40766d30889b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "720a15570b957f572875637562e553fe",
+    "url": "/react-superheros/index.html"
+  },
+  {
+    "revision": "377f9f09e75383c480a2",
+    "url": "/react-superheros/static/css/main.a0dd50bf.chunk.css"
+  },
+  {
+    "revision": "9adf46f851042a612df9",
+    "url": "/react-superheros/static/js/2.c5eb0419.chunk.js"
+  },
+  {
+    "revision": "377f9f09e75383c480a2",
+    "url": "/react-superheros/static/js/main.47e59356.chunk.js"
+  },
+  {
+    "revision": "76f220998ebea97ba914",
+    "url": "/react-superheros/static/js/runtime-main.1e50cb4c.js"
+  }
+]);
